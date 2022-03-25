e783894c2312fb01da692bc6baf7408734b1d016
03yangl <44915452+03yangl@users.noreply.github.com>
2022-03-25 16:58:49 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+// App.js
+import { Component } from 'react/cjs/react.production.min';
+import './App.css';
+import FetchData from './FetchData';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <FetchData />
+    </div>
+    );
+  }
+}
+
+export default App;
